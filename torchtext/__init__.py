b21ca3c809101a83c11d6766c70ba54a33e098d9
Marc Wang <474733787@qq.com>
2018-06-22 23:32:21 +0800

    del

--- torchtext/__init__.py
+++ torchtext/__init__.py
@@ -1,12 +1,12 @@
-# from . import data
-# from . import datasets
-# from . import utils
-import data
-import datasets
-import utils
-#
-# __version__ = '0.3.0'
-#
-# __all__ = ['data',
-#            'datasets',
-#            'utils']
+from . import data
+from . import datasets
+from . import utils
+# import data
+# import datasets
+# import utils
+
+__version__ = '0.3.0'
+
+__all__ = ['data',
+           'datasets',
+           'utils']
