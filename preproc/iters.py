617fbca3c6301b1dfc79cb4ee0dcbf7ea37135d8
Marc Wang <474733787@qq.com>
2018-06-27 23:07:14 +0800

    char level

--- preproc/iters.py
+++ preproc/iters.py
@@ -8,8 +8,8 @@ PAD_WORD = '<blank>'
 UNK_WORD = '<unk>'
 BOS_WORD = '<s>'
 
-HOME=os.path.abspath('.')
-# HOME=os.path.abspath('..')
+# HOME=os.path.abspath('.')
+HOME=os.path.abspath('..')
 DATA=os.path.join(HOME,'data_folder')
 
 jieba.load_userdict(os.path.join(DATA,'dict.txt'))
@@ -22,7 +22,7 @@ def tokenizer_char(txt):
 
 def build_iters(ftrain='train.tsv',fvalid='valid.tsv',bsz=64, level='char'):
     TEXT = torchtext.data.Field(sequential=True,
-                                tokenize=tokenizer_word if level == 'word' else 'char',
+                                tokenize=tokenizer_word if level == 'word' else tokenizer_char,
                                 pad_token=PAD_WORD, unk_token=UNK_WORD)
     LABEL = torchtext.data.Field(sequential=False, use_vocab=False)
 
@@ -51,4 +51,6 @@ def build_iters(ftrain='train.tsv',fvalid='valid.tsv',bsz=64, level='char'):
     return TEXT, LABEL, train_iter, valid_iter
 
 if __name__ == '__main__':
-    print('')
+    TEXT, LABEL, train_iter, valid_iter = build_iters()
+    for sample in train_iter:
+        print(sample.seq1)
