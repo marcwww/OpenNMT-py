b39f7a1f24ed3213aa3dc14cfaffc2921ebdb7c0
Marc Wang <474733787@qq.com>
2018-06-29 22:02:11 +0800

    stick back

--- data_folder/stick_back.py
+++ data_folder/stick_back.py
@@ -1,12 +1,18 @@
 
 atec_train = 'atec_train.txt'
 atec_valid = 'atec_valid.txt'
+seqs1=[]
+seqs2=[]
+lbls=[]
 
 with open(atec_train, 'r') as f_in, open(atec_train+'.back', 'w') as f_out:
     for line in f_in:
         seq1, seq2, lbl=line.split('\t')
         seq1 = ''.join(seq1.split(' '))
         seq2 = ''.join(seq2.split(' '))
+        seqs1.append(seq1)
+        seqs2.append(seq2)
+        lbls.append(lbl)
         f_out.write(seq1+'\t'+seq2+'\t'+lbl)
 
 with open(atec_valid, 'r') as f_in, open(atec_valid+'.back', 'w') as f_out:
@@ -14,4 +20,11 @@ with open(atec_valid, 'r') as f_in, open(atec_valid+'.back', 'w') as f_out:
         seq1, seq2, lbl=line.split('\t')
         seq1 = ''.join(seq1.split(' '))
         seq2 = ''.join(seq2.split(' '))
+        seqs1.append(seq1)
+        seqs2.append(seq2)
+        lbls.append(lbl)
         f_out.write(seq1+'\t'+seq2+'\t'+lbl)
+
+with open(atec_train+'.'+atec_valid+'.back','w') as f_out:
+    for seq1,seq2,lbl in zip(seqs1,seqs2,lbls):
+        f_out.write(seq1 + '\t' + seq2 + '\t' + lbl)
