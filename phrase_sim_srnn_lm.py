9ad8c878f7d829b1dbde03514543fb1ff7afb45a
Marc Wang <474733787@qq.com>
2018-07-14 01:22:50 +0800

    srnn_lm

--- phrase_sim_srnn_lm.py
+++ phrase_sim_srnn_lm.py
@@ -12,11 +12,11 @@ from sklearn import metrics
 import logging
 import numpy as np
 from onmt.utils import optimizers
-from torch.nn import functional as F
+import sys
 import crash_on_ipy
+from torch.nn import functional as F
 
 LOGGER = logging.getLogger(__name__)
-SAVE_PER = 10
 
 NACT=3
 NONLINEAR=nn.Tanh
@@ -33,54 +33,119 @@ class Avg(nn.Module):
 
         return mem_bank.sum(dim=0)/len
 
-class FourWay(nn.Module):
+class MultiWay(nn.Module):
+
+    def __init__(self):
+        super(MultiWay, self).__init__()
+        self.nways = 3
 
     def forward(self, u1, u2):
-        way1 = u1
-        way2 = u2
+        # way1 = u1
+        # way2 = u2
         way3 = torch.abs(u1-u2)
         way4 = u1*u2
+        way5 = torch.max(torch.stack([u1*u1,u2*u2]), dim=0)[0]
 
-        return torch.cat([way1,way2,way3,way4],dim=1)
+        # return torch.cat([way1,way2,way3,way4],dim=1)
+        return torch.cat([way3,way4,way5],dim=1)
 
 
 class PhraseSim(nn.Module):
 
-    def __init__(self, encoder, device):
+    def __init__(self, encoder, dropout):
         super(PhraseSim, self).__init__()
         self.encoder = encoder
         self.avg = Avg()
-        self.fourway =FourWay()
+        self.MultiWay =MultiWay()
         self.generator = nn.Sequential(
-            nn.Linear(4*encoder.odim,1*encoder.odim),
+            nn.Linear(self.MultiWay.nways * encoder.odim,
+                      1 * encoder.odim),
             nn.ReLU(),
-            nn.Linear(1*encoder.odim,1),
-            nn.Sigmoid())
-        self.device = device
+            nn.Dropout(dropout),
+            nn.Linear(1*encoder.odim, 2),
+            nn.Softmax())
+        self.dropout = nn.Dropout(dropout)
+        if not encoder.bidirection:
+            self.W_dis = nn.\
+                Parameter(torch.Tensor(encoder.odim,
+                                       encoder.odim))
+        else:
+            self.W_dis = nn. \
+                Parameter(torch.Tensor(encoder.odim/2,
+                                       encoder.odim/2))
 
     def forward(self, seq1, seq2):
         # seq1 = seq1.unsqueeze(2)
         # seq2 = seq2.unsqueeze(2)
 
-        bsz = seq1.shape[1]
-        hidden = \
-            self.encoder.\
-                init_hidden(bsz, self.device)
-        stacks = \
-            self.encoder.\
-                init_stack(bsz, self.device)
+        outputs1, hidden1, stacks1 = self.encoder(seq1)
+        outputs2, hidden2, stacks2 = self.encoder(seq2)
 
-        _, hidden1, stacks = self.encoder(seq1, hidden, stacks)
-        _, hidden2, stacks = self.encoder(seq2, hidden, stacks)
-
-        cat_res = self.fourway(hidden1,hidden2)
+        cat_res = self.MultiWay(hidden1, hidden2)
+        cat_res = self.dropout(cat_res)
 
         probs = self.generator(cat_res)
+        we_T = self.encoder.embedding.weight.transpose(0, 1)
+        if self.encoder.bidirection:
+            # we_T = torch.stack([we_T, we_T], dim=0).\
+            #     view(-1, self.encoder.embedding.num_embeddings)
+            outputs1 = outputs1[:, :, :self.encoder.odim / 2]
+            outputs2 = outputs2[:, :, :self.encoder.odim / 2]
 
-        return probs
+        logits1 = torch.matmul(outputs1, self.W_dis.matmul(we_T))
+        logits2 = torch.matmul(outputs2, self.W_dis.matmul(we_T))
 
-# def create_stack(stack_size,stack_elem_size):
-#     return np.array([([EMPTY_VAL] * stack_elem_size)] * stack_size)
+        return probs, logits1, logits2
+
+class Attention(nn.Module):
+
+    def __init__(self, hdim):
+        super(Attention, self).__init__()
+        self.hdim = hdim
+        self.generator = nn.Sequential(
+            nn.Linear(hdim, hdim),
+            nn.Tanh(),
+            nn.Linear(hdim, 1),
+            # nn.Softmax(dim=0)
+        )
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, inputs, mask):
+        a_raw = self.generator(inputs)
+        a_raw.masked_fill_(mask.unsqueeze(-1), -float('inf'))
+        a = self.softmax(a_raw)
+        return (inputs * a).sum(dim=0)
+
+class Encoder(nn.Module):
+
+    def __init__(self, voc_size, hdim, padding_idx,
+                 n_layers=1, dropout=0.5, bidirection=False):
+        super(Encoder, self).__init__()
+        self.hdim = hdim
+        self.embedding = nn.Embedding(voc_size,
+                                      hdim,
+                                      padding_idx=padding_idx)
+        self.padding_idx = padding_idx
+        self.n_layers = n_layers
+        self.bidirection = bidirection
+        self.gru = nn.GRU(hdim, hdim, n_layers,
+                          dropout, bidirectional=bidirection)
+        self.odim = hdim * 2 if bidirection else hdim
+        self.dropout = nn.Dropout(dropout)
+        self.attention = Attention(self.odim)
+
+    def forward(self, inputs, hidden=None):
+        embs = self.embedding(inputs)
+        mask = inputs.data.eq(self.padding_idx)
+        mask_embs = mask.unsqueeze(-1).expand_as(embs)
+        embs.masked_fill_(mask_embs, 0)
+
+        embs = self.dropout(embs)
+
+        outputs, hidden = self.gru(embs, hidden)
+        final_hidden = self.attention(outputs, mask)
+
+        return outputs, final_hidden
 
 def shift_matrix(n):
     W_up=np.eye(n)
@@ -95,7 +160,8 @@ def shift_matrix(n):
 
 class EncoderSRNN(nn.Module):
     def __init__(self, input_size, hidden_size,
-                 nstack, stack_depth, stack_size,
+                 padding_idx, nstack,
+                 stack_depth, stack_size,
                  stack_elem_size):
         super(EncoderSRNN, self).__init__()
         # here input dimention is equal to hidden dimention
@@ -104,12 +170,13 @@ class EncoderSRNN(nn.Module):
         self.stack_size=stack_size
         self.stack_depth=stack_depth
         self.stack_elem_size=stack_elem_size
-        # self.embedding = nn.Embedding(input_size,
-        #                               hidden_size,
-        #                               padding_idx=PAD)
         self.embedding = nn.Embedding(input_size,
-                                      hidden_size)
-
+                                      hidden_size,
+                                      padding_idx=padding_idx)
+        self.padding_idx = padding_idx
+        # self.embedding = nn.Embedding(input_size,
+        #                               hidden_size)
+        #
         self.nonLinear=NONLINEAR()
         self.hid2hid=nn.Linear(hidden_size,hidden_size)
         self.input2hid=nn.Linear(hidden_size,hidden_size)
@@ -126,6 +193,7 @@ class EncoderSRNN(nn.Module):
         self.W_down = nn.Parameter(torch.Tensor(W_down))
 
         self.odim = hidden_size
+        self.attention = Attention(self.odim)
 
     def update_stack(self, stacks,
                      p_push, p_pop, p_noop, push_vals):
@@ -154,35 +222,36 @@ class EncoderSRNN(nn.Module):
 
         return stacks
 
-    def forward(self, inputs, hidden, stacks):
+    def forward(self, inputs):
+
+        bsz = inputs.shape[1]
+        hidden = \
+            self.encoder. \
+                init_hidden(bsz, self.device)
+        stacks = \
+            self.encoder. \
+                init_stack(bsz, self.device)
+
         # inputs: length * bsz
         # stacks: bsz * nstack * stacksz * stackelemsz
         embs = self.embedding(inputs)
         # inputs(length,bsz)->embd(length,bsz,embdsz)
+        mask = inputs.data.eq(self.padding_idx)
+        mask_embs = mask.unsqueeze(-1).expand_as(embs)
+        embs.masked_fill_(mask_embs, 0)
 
+        embs = self.dropout(embs)
         # batch_size=inputs.shape[1]
 
         outputs=[]
         for input in embs:
             # input: bsz * embdsz
-            # input=self.input2hid(input)
-
             # hidden: bsz * hidden_size
             cur_hidden=self.gru(input,hidden)
 
 
             if USE_STACK:
                 # # stack_vals: bsz * nstack * (stack_depth * stack_elem_size)
-                # stack_vals = stacks[:, :, :self.stack_depth, :].contiguous(). \
-                #     view(batch_size,
-                #          self.nstack,
-                #          self.stack_depth * self.stack_elem_size)
-                #
-                # # read_res: bsz * nstack * hidden_size
-                # read_res = self.read_stack(stack_vals)
-                # # read_res: bsz * (nstack * hidden_size)
-                # read_res = read_res.view(batch_size, -1)
-
                 act = self.hid2act(hidden)
                 act = act.view(-1, self.nstack, NACT)
                 # act: bsz * nstack * 3
@@ -201,7 +270,10 @@ class EncoderSRNN(nn.Module):
             output = hidden
             outputs.append(output)
 
-        return outputs, hidden, stacks
+        outputs = torch.stack(outputs)
+        final_hidden = self.attention(outputs, mask)
+
+        return outputs, final_hidden, stacks
 
     def init_stack(self, batch_size, device):
         return self.empty_elem.expand(batch_size,
@@ -209,6 +281,7 @@ class EncoderSRNN(nn.Module):
                                       self.stack_size,
                                       self.stack_elem_size).\
                                       contiguous().to(device)
+
     def init_hidden(self, batch_size, device):
         return torch.zeros(batch_size,self.hidden_size).to(device)
 
@@ -239,7 +312,9 @@ def save_checkpoint(model, epoch,
     # LOGGER.info("Saving model training history to '%s'", train_fname)
     print("Saving model training history to '%s'" % (train_fname))
     content = {
-        'loss': losses,
+        'loss': losses['loss'],
+        'loss_ps': losses['loss_ps'],
+        'loss_lm': losses['loss_lm'],
         'accuracy': accurs,
         'precs': precs,
         'recalls': recalls,
@@ -247,7 +322,7 @@ def save_checkpoint(model, epoch,
     }
     open(train_fname, 'wt').write(json.dumps(content))
 
-def train_batch(sample, model, criterion, optim, class_weight):
+def train_batch(sample, model, criterion, optim, lm_coef):
     model.train()
 
     model.zero_grad()
@@ -257,28 +332,26 @@ def train_batch(sample, model, criterion, optim, class_weight):
 
     seq1 = seq1.to(device)
     seq2 = seq2.to(device)
-    lbl = lbl.type(torch.FloatTensor)
-
-    bs_weight = lbl.clone().\
-        apply_(lambda x:class_weight['wneg'] if x == 0 else class_weight['wpos'])
-
-    criterion.weight = bs_weight.to(device)
-
+    tar1 = seq1[1:]
+    tar2 = seq2[1:]
     lbl = lbl.to(device)
     # seq : (seq_len,bsz)
     # lbl : (bsz)
-    probs = model(seq1, seq2)
+    probs, logits1, logits2 = model(seq1[:-1], seq2[:-1])
+    voc_size = model.encoder.embedding.num_embeddings
     # decoder_outputs : (1,bsz,hdim)
     # probs : (bsz)
 
-    loss = criterion(probs, lbl)
+    loss_ps = criterion['ps'](probs, lbl)
+    loss_lm1 = criterion['lm'](logits1.view(-1, voc_size), tar1.view(-1))
+    loss_lm2 = criterion['lm'](logits2.view(-1, voc_size), tar2.view(-1))
+    loss = (loss_ps + lm_coef * (loss_lm1+loss_lm2)/2)/(1+lm_coef)
     loss.backward()
-    # print(sum-param_sum(model.parameters()),sum,param_sum(model.parameters()))
-    # sum=param_sum(model.parameters())
-    # clip_grads(model)
     optim.step()
 
-    return loss
+    return {'loss':loss.data.item(),
+            'ps':loss_ps.data.item(),
+            'lm':((loss_lm1+loss_lm2)/2).data.item()}
 
 def restore_log(opt):
     basename = "{}-epoch-{}".format(opt.exp, opt.load_idx)
@@ -289,13 +362,18 @@ def restore_log(opt):
            history['precs'],history['recalls'],history['f1s']
 
 def train(train_iter, val_iter, epoch, model,
-          optim, criterion, opt, class_weight):
-    # sum=param_sum(model.parameters())
+          optim, criterion, opt):
+
     losses=[]
+    losses_ps = []
+    losses_lm = []
     accurs=[]
     f1s=[]
     precs=[]
     recalls=[]
+    losses_log=[]
+    losses_ps_log=[]
+    losses_lm_log = []
 
     if opt.load_idx != -1:
         losses,accurs,\
@@ -304,31 +382,39 @@ def train(train_iter, val_iter, epoch, model,
 
     epoch_start = epoch['start']
     epoch_end = epoch['end']
-
-    # valid(val_iter,model)
+    save_per = epoch['save_per']
 
     for epoch in range(epoch_start,epoch_end):
         nbatch = 0
         for i, sample in enumerate(train_iter):
             nbatch += 1
 
-            loss = train_batch(sample,model,criterion,optim,class_weight)
-
-            loss_val = loss.data.item()
-            losses.append(loss_val)
+            loss = train_batch(sample,model,criterion,optim,opt.lm_coef)
+            losses.append(loss['loss'])
+            losses_ps.append(loss['ps'])
+            losses_lm.append(loss['lm'])
             percent = (i+.0)/len(train_iter)
-            progress_bar(percent,loss_val,epoch)
+            progress_bar(percent,loss['loss'],epoch)
 
         accurracy, precision, recall, f1 = valid(val_iter,model)
-        print("Valid: accuracy:%.3f precision:%.3f recall:%.3f f1:%.3f avg_loss:%.4f" %
-              (accurracy, precision, recall, f1, np.array(losses[-nbatch:]).mean()))
-        accurs.extend([accurracy for _ in range(nbatch)])
-        precs.extend([precision for _ in range(nbatch)])
-        recalls.extend([recall for _ in range(nbatch)])
-        f1s.append([f1 for _ in range(nbatch)])
+        loss_mean = np.array(losses[-nbatch:]).mean()
+        loss_ps_mean = np.array(losses_ps[-nbatch:]).mean()
+        loss_lm_mean = np.array(losses_lm[-nbatch:]).mean()
+        print("Valid: accuracy:%.3f precision:%.3f recall:%.3f f1:%.3f avg_loss(total/ps/lm):%.4f/%.4f/%.4f" %
+              (accurracy, precision, recall, f1, loss_mean, loss_ps_mean, loss_lm_mean))
+        accurs.append(accurracy)
+        precs.append(precision)
+        recalls.append(recall)
+        f1s.append(f1)
+        losses_log.append(loss_mean)
+        losses_ps_log.append(loss_ps_mean)
+        losses_lm_log.append(loss_lm_mean)
 
-        if (epoch+1) % SAVE_PER == 0:
-            save_checkpoint(model,epoch,losses,accurs,precs,recalls,f1s,opt.exp)
+        if (epoch+1) % save_per == 0:
+            save_checkpoint(model,epoch,{'loss':losses_log,
+                                         'loss_ps':losses_ps_log,
+                                         'loss_lm':losses_lm_log},
+                            accurs,precs,recalls,f1s,opt.exp)
 
 def valid(val_iter, model):
     model.eval()
@@ -347,9 +433,9 @@ def valid(val_iter, model):
 
             # seq : (seq_len,bsz)
             # lbl : (bsz)
-            probs = model(seq1, seq2)
+            probs, _, _ = model(seq1, seq2)
             # probs : (bsz)
-            pred = probs.cpu().apply_(lambda x: 0 if x < 0.5 else 1)
+            pred = probs.max(dim=1)[1].cpu()
             pred_lst.extend(pred.numpy())
             lbl_lst.extend(lbl.numpy())
 
@@ -412,27 +498,42 @@ if __name__ == '__main__':
     opts.add_md_help_argument(parser)
     opts.model_opts(parser)
     opts.train_opts(parser)
-
     opt = parser.parse_args()
+    with open('%s.arg' % opt.exp, 'w') as f:
+        f.write(' '.join(sys.argv[1:]))
 
     TEXT, LALEBL, train_iter, valid_iter = \
-        iters.build_iters(ftrain='train.tsv', bsz=opt.batch_size)
+        iters.build_iters_lm(ftrain=opt.ftrain, fvalid=opt.fvalid,
+                             bsz=opt.batch_size, level=opt.level,
+                             min_freq=opt.min_freq)
 
     class_probs = dataset_bias(train_iter)
     print('Class probs: ', class_probs)
-    cweights = class_weight(class_probs, opt.label_smoothing)
+    # cweights = class_weight(class_probs, opt.label_smoothing)
+    cweights = {
+        'wneg': 1 - opt.pos_weight,
+        'wpos': opt.pos_weight
+    }
     print('Class weights: ', cweights)
 
     location = opt.gpu if torch.cuda.is_available() and opt.gpu != -1 else 'cpu'
     device = torch.device(location)
+    # encoder = Encoder(len(TEXT.vocab.stoi),
+    #                   opt.rnn_size,
+    #                   TEXT.vocab.stoi[PAD_WORD],
+    #                   opt.enc_layers,
+    #                   opt.dropout,
+    #                   opt.bidirection)
+
     encoder = EncoderSRNN(len(TEXT.vocab.stoi),
                           opt.rnn_size,
+                          TEXT.vocab.stoi[PAD_WORD],
                           opt.nstack,
                           opt.stack_depth,
                           opt.stack_size,
                           opt.stack_elem_size)
 
-    model = PhraseSim(encoder, device).to(device)
+    model = PhraseSim(encoder, opt.dropout).to(device)
     init_model(opt, model)
 
     # print(model.state_dict())
@@ -446,9 +547,14 @@ if __name__ == '__main__':
 
     # model.generator = generator.to(device)
     optim = optimizers.build_optim(model, opt, None)
-    criterion = nn.BCELoss(size_average=True)
-    epoch = {'start': opt.load_idx if opt.load_idx != -1 else 0,
-             'end': 10000}
+    weight = torch.Tensor([cweights['wneg'], cweights['wpos']]).to(device)
+    criterion_ps = nn.CrossEntropyLoss(weight=weight)
+    criterion_lm = nn.CrossEntropyLoss(ignore_index=TEXT.vocab.stoi[iters.PAD_WORD])
+    criterion = {'ps': criterion_ps,
+                 'lm': criterion_lm}
+    epoch = {'start': opt.load_idx + 1 if opt.load_idx != -1 else 0,
+             'end': opt.nepoch,
+             'save_per': opt.save_per}
 
     train(train_iter, valid_iter, epoch,
-          model, optim, criterion, opt, cweights)
+          model, optim, criterion, opt)
